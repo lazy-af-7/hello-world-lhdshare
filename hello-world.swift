e01431b6940257089c3e29ca8f6d3e5d1f212792
lazy-af-7 <honey.batra007@icloud.com>
2021-03-29 13:14:12 +0530

    hello world in swift

---
+++ hello-world.swift
@@ -0,0 +1,9 @@
+func helloWorld(){
+    print("Hello World!")
+}
+func greet(user:String, location:String){
+    print("Hello \(user)! How's the weather in \(location).")
+}
+helloWorld()
+var name = "Garvit"
+greet(user:name,location:"Cupertino")
